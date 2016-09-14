663a5515f85048e96662abbfc33e98821ffd2c13
mastermarkhun <mastermark@outlook.com>
2016-09-14 22:19:23 +0200

    Delay for sleep and parameters

--- caseled/caseled.ino
+++ caseled/caseled.ino
@@ -4,18 +4,20 @@ const int led = 9;
 int idle = 10;
 int active = 64;
 int incomingByte;
+bool firstTry = true;
 
 void setup() {
   Serial.begin(9600);
   pinMode(led, OUTPUT);
   pinMode(A0,INPUT);
   pinMode(A1,INPUT);
-  pulse();
+  pulse(255, 1);
 }
 
 void loop() {
   int pwrSensor = digitalRead(A1);
   if (pwrSensor == HIGH) {
+    firstTry = true;
     analogWrite(led, idle);
     hddActivity();
     serialRead();
@@ -38,34 +40,33 @@ void serialRead() {
     if (incomingByte == 0) {
       active = 0;
       idle = 0;
-      pulse();
+      pulse(255, 1);
     } else {
       active = 64;
       idle = 10;
-      pulse();
+      pulse(255, 1);
     }
   }
 }
 
-void pulse() {
-  for (int x = 0; x <= 255; x++) {
+void pulse(int a, int b) {
+  for (int x = 0; x <= a; x++) {
     analogWrite(led, x);
-    delay(1);
+    delay(b);
   }
-  for (int x = 254; x >= 0; x--) {
+  for (int x = a-1; x >= 0; x--) {
     analogWrite(led, x);
-    delay(1);
+    delay(b);
   }
 }
 
 void slowPulse() {
-  for (int x = 0; x <= 64; x++) {
-    analogWrite(led, x);
-    delay(50);
-  }
-  for (int x = 63; x >= 0; x--) {
-    analogWrite(led, x);
-    delay(50);
+  if (firstTry == true) {
+    firstTry = false;
+    analogWrite(led, 0);
+    delay(5000);
+  } else {
+    pulse(64, 50);
   }
   delay(250);
 }
