16b41520cc37ad178a39a3076b0ab371ffd28745
mastermarkhun <idna.mark@gmail.com>
2018-07-14 08:54:57 +0200

    Initial commit

---
+++ unifi-scheduler/script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#description=Scheduled switching for Unifi AP LED.
+#foregroundOnly=false
+#backgroundOnly=false
+#arrayStarted=true
+#name=Unifi LED switcher
+#argumentDescription=
+#argumentDefault=
+
+time=$( date +%H )
+
+if [ $time -ge 22 ] || [ $time -lt 06 ]
+then
+    bool="false"
+else
+    bool="true"
+fi
+
+echo "time: $time"
+echo "bool: $bool"
+
+/usr/bin/expect <<EOD
+
+set timeout 60
+
+spawn ssh <username>@<ip-address>
+
+expect "yes/no" { 
+	send "yes\r"
+	expect "*?assword" { send "<password>\r" }
+	} "*?assword" { send "<password>\r" }
+
+expect "# " { send "echo mgmt.led_enabled=$bool > /var/etc/persistent/cfg/mgmt\r" }
+interact
