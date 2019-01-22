faf152cb370ab2264a3cd46560ad26bcd738e234
mastermarkhun <idna.mark@gmail.com>
2019-01-22 23:14:03 +0100

    Initial commit

---
+++ plex-sort/plexsort.ps1
@@ -0,0 +1,15 @@
+﻿param (
+    [string]$folder,
+    [string]$plexfolder
+)
+
+$items = Get-ChildItem $folder | Sort-Object -Property "CreationTime"
+
+foreach ($item in $items)
+{
+    Get-ItemProperty -Path $item | %{Write-Verbose -Verbose "$($_.CreationTime)`t$item"}
+
+    Move-Item -Path $item -Destination $plexfolder
+
+    Start-Sleep -Seconds 30
+}
