067a1f8b1d694baaf7b70cae990ab86a7295e81c
mastermarkhun <idna.mark@gmail.com>
2020-05-15 19:20:54 +0200

    Create deploy.ps1

---
+++ secret/deploy.ps1
@@ -0,0 +1,100 @@
+$domain = "contoso.com"
+$domainDN = "DC=contoso,DC=com"
+
+$names = @(
+    'Tawnya Trial',
+    'Julietta Johnsen',
+    'Kira Kua',
+    'Stephen Shofner',
+    'Kristine Kubota',
+    'Denis Damico',
+    'Guillermo Gustavson',
+    'Carmelo Cabe',
+    'Danny Desir',
+    'Mila Munsterman'
+)
+
+$OrgUnits = @(
+    'Sales and Marketing',
+    'Product Management',
+    'Operations',
+    'Legal Division',
+    'Finance Division',
+    'New products Division'
+)
+
+$OperationsGroups = @(
+    'Technical Operations',
+    'Information Technologies',
+    'Floor Operations',
+    'Buisness Intelligence'
+)
+
+$InformationTechnologiesGroups = @(
+    'Quality Assurance',
+    'Development',
+    'Platform Development',
+    'IT Services'
+)
+
+$DomainAdminGroups = @(
+    'Global Admins',
+    'Operation Admins',
+    'Services',
+    'Temp'
+)
+
+Import-Module ActiveDirectory
+Add-Type -AssemblyName 'System.Web'
+
+$OrgUnits | foreach {
+    New-ADOrganizationalUnit -Name $_ -Path $domainDN
+}
+
+foreach ($person in $names)
+{
+    $splitname = $person.Split(" ")
+    $samaccname = $splitname[0].Substring(0,3) + $splitname[1].Substring(0,2)
+
+    $length = 5 ## characters
+    $nonAlphaChars = 0
+    $password = [System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars)
+    $secPw = ConvertTo-SecureString -String $password -AsPlainText -Force
+
+    New-ADUser -Name $person -GivenName $splitname[0] -Surname $splitname[1] -SamAccountName $samaccname -UserPrincipalName "$samaccname@$domain" -Path "OU=$($OrgUnits|Get-Random),$domainDN" -AccountPassword $secPw -ChangePasswordAtLogon $false -Enabled $true
+
+    "$samaccname,$password" | Out-File -FilePath "pwd.csv" -Append -Force
+}
+
+foreach ($groupname in $OperationsGroups)
+{
+    $samaccname = @($groupname -replace '\s','')
+
+    New-ADGroup -Name $groupname -SamAccountName $samaccname -GroupCategory Security -GroupScope Global -DisplayName $groupname -Path "OU=Operations,$domainDN"
+}
+
+foreach ($groupname in $InformationTechnologiesGroups)
+{
+    $samaccname = @($groupname -replace '\s','')
+
+    New-ADGroup -Name $groupname -SamAccountName $samaccname -GroupCategory Security -GroupScope Global -DisplayName $groupname -Path "CN=Users,$domainDN"
+}
+
+foreach ($groupname in $DomainAdminGroups)
+{
+    $samaccname = @($groupname -replace '\s','')
+
+    New-ADGroup -Name $groupname -SamAccountName $samaccname -GroupCategory Security -GroupScope Global -DisplayName $groupname -Path "CN=Users,$domainDN"
+}
+
+Add-ADGroupMember -Identity 'InformationTechnologies' -Members $InformationTechnologiesGroups
+
+foreach ($group in $OperationsGroups)
+{
+    Add-ADGroupMember -Identity $group -Members @($names | Get-Random -Count 2)
+}
+
+Add-ADGroupMember -Identity 'IT Services' -Members @($names | Get-Random)
+
+Add-ADGroupMember -Identity 'Domain Admins' -Members 'InformationTechnologies'
+Add-ADGroupMember -Identity 'Domain Admins' -Members $DomainAdminGroups
